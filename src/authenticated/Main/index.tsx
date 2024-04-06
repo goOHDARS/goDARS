3b4ffdf89c8e740b000694be9ba54ef96795ed01
Zachary Wolfe <zacharywolfe29@gmail.com>
2024-04-06 17:19:13 -0400

    separate modal && setup for soon to be user data

--- src/authenticated/Main/index.tsx
+++ src/authenticated/Main/index.tsx
@@ -7,12 +7,18 @@
 
 import React, { useState } from "react";
 import { View, StyleSheet, Text, ScrollView, Modal, Pressable } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Main(){
     const [modalVisible, setModalVisible] = useState(false);
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
+            <SafeAreaView>
+                <Text>
+                    Your mom
+                </Text>
+            </SafeAreaView>
             <Modal animationType="slide" transparent={false} visible={modalVisible} onRequestClose={() => setModalVisible(!modalVisible)}>
                 <View style={styles.centeredContent}>
                     <Pressable onPress={() => setModalVisible(false)}>
@@ -22,15 +28,48 @@ export default function Main(){
                     </Pressable>
                 </View>
             </Modal>
-            <Pressable style={{backgroundColor: "#024230"}} onPress={() => setModalVisible(true)}>
-                <View style={{minWidth: "75%", height: "35%", borderRadius: 20, alignItems: "center"}}>
-                    <Text style={{color: "#fff"}}>
-                        Fuck React Native
-                    </Text>
+            <Modal animationType="slide" transparent={false} visible={modalVisible} onRequestClose={() => setModalVisible(!modalVisible)}>
+                <View style={styles.centeredContent}>
+                    <Pressable onPress={() => setModalVisible(false)}>
+                        <Text>
+                            Hello World
+                        </Text>
+                    </Pressable>
                 </View>
-            </Pressable>
-        </View>
-        );
+            </Modal>
+            <ScrollView>
+                <SafeAreaView>
+                    <View style={{paddingBottom: 20}}>
+                        <Pressable style={{backgroundColor: "#024230", minWidth: "85%", minHeight: 300, borderRadius: 20, alignItems: "center"}} onPress={() => setModalVisible(true)}>
+                            <Text style={{color: "#fff"}}>
+                                Fuck React Native
+                            </Text>
+                        </Pressable>
+                    </View>
+                    <View style={{paddingBottom: 20}}>
+                        <Pressable style={{backgroundColor: "#024230", minWidth: "85%", minHeight: 300, borderRadius: 20, alignItems: "center"}} onPress={() => setModalVisible(true)}>
+                            <Text style={{color: "#fff"}}>
+                                Fuck React Native
+                            </Text>
+                        </Pressable>
+                    </View>
+                    <View style={{paddingBottom: 20}}>
+                        <Pressable style={{backgroundColor: "#024230", minWidth: "85%", minHeight: 300, borderRadius: 20, alignItems: "center"}} onPress={() => setModalVisible(true)}>
+                            <Text style={{color: "#fff"}}>
+                                Fuck React Native
+                            </Text>
+                        </Pressable>
+                    </View>
+                    <View style={{paddingBottom: 20}}>
+                        <Pressable style={{backgroundColor: "#024230", minWidth: "85%", minHeight: 300, borderRadius: 20, alignItems: "center"}} onPress={() => setModalVisible(true)}>
+                            <Text style={{color: "#fff"}}>
+                                Fuck React Native
+                            </Text>
+                        </Pressable>
+                    </View>
+                </SafeAreaView>
+            </ScrollView>
+        </View>);
 }
 
 const styles = StyleSheet.create({
