6a5d391164e2cf4bd9df8ddea599cdc816024c61
Reem-Lamsettef <rl891621@ohio.edu>
2024-04-06 16:10:30 -0400

    Icons for tabs added

--- App.tsx
+++ App.tsx
@@ -7,6 +7,8 @@ import Search from './src/authenticated/Search';
 import Schedule from './src/authenticated/Schedule';
 import Chat from './src/authenticated/Chat';
 import MyTabBar from './src/authenticated/TabBar';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 // import MyTabBar from './src/authenticated/TabBar';
 
 
@@ -34,7 +36,33 @@ export default function App() {
 			})} */
 			
 			}
-			<Tab.Navigator tabBarPosition='bottom' initialRouteName='Home' style={styles.userData} screenOptions={{ tabBarLabelStyle: {fontSize: 11, fontWeight: '600'}}}>
+			<Tab.Navigator tabBarPosition='bottom' style={styles.userData}
+				screenOptions={({ route }) => ({
+					//tabBarLabelStyle: {fontSize: 11, fontWeight: '600'}
+					tabBarIcon: ({ focused, color}) => {
+					  let iconName;
+					  let size = 26;
+					  if (route.name === 'Home') {
+						iconName = focused ? 'home' : 'home-outline';
+					  } else if (route.name === 'Search') {
+						iconName = focused ? 'search-circle-sharp' : 'search-circle-outline';
+					  }
+					  else if (route.name === "Schedule"){
+						iconName = focused ? 'document-text-sharp' : 'document-text-outline';
+					  }
+					  else{
+						iconName = focused ? 'chatbubble-ellipses-sharp' : 'chatbubble-ellipses-outline';
+					  }
+		  
+					  // You can return any component that you like here!
+					  return <Ionicons name={iconName} size={size} color={color} />;
+					},
+					tabBarActiveTintColor: 'green',
+					tabBarInactiveTintColor: 'gray',
+				  })}
+		  
+			
+			>
 				<Tab.Screen name='Home' component={Main} />
 				<Tab.Screen name='Search' component={Search} />
 				<Tab.Screen name='Schedule' component={Schedule} />
