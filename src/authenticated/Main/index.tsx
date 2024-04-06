475b79a58cc03792425ccd8ee926489969915d24
Zachary Wolfe <zacharywolfe29@gmail.com>
2024-04-06 14:45:47 -0400

    fuck react native

---
+++ src/authenticated/Main/index.tsx
@@ -0,0 +1,17 @@
+/**
+ * @file     Main.tsx
+ * @author   Zachary Wolfe (zw224021@ohio.edu)
+ * @brief    A file for the Main screen component
+ * @date     April 06, 2024
+*/
+
+import React from "react";
+import { View, StyleSheet, Text, ScrollView, Modal } from "react-native";
+
+export default function Main(){
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Home!</Text>
+        </View>
+        );
+}
